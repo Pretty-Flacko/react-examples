d8ef6b2687c6380330c353aad515b1a8a1f2b408
Pretty-Flacko <48690875+Pretty-Flacko@users.noreply.github.com>
2026-06-20 16:01:50 +0400

    Add a counter for number of background changes

--- state/src/App.jsx
+++ state/src/App.jsx
@@ -5,9 +5,11 @@ const COLORS = ['pink', 'green', 'blue', 'yellow', 'purple'];
 
 function App() {
   const [backgroundColor, setBackgroundColor] = useState(COLORS[0]);
+  const [numberOfChanges, setNumberOfChanges] = useState(0);
 
   const onButtonClick = (color) => () => {
     setBackgroundColor(color);
+    setNumberOfChanges(numberOfChanges + 1);
   };
 
   return (
@@ -17,6 +19,7 @@ function App() {
         backgroundColor,
       }}
     >
+      <div className="counter">Number of changes: {numberOfChanges}</div>
       {COLORS.map((color) => (
         <button
           type="button"
